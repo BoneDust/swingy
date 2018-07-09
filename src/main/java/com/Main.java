31277a0d2e5e93820d76afac9af13a44984e1077
BoneDust <gtshekel@student.wethinkcode.co.za>
2018-07-10 01:04:30 +0200

    updated main. still in test phase

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,20 +1,35 @@
 package com;
 
-
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
+import javax.validation.ValidatorFactory;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ConstraintViolation;
+import java.util.Set;
 
 @Getter
 @Setter
 public class Main
 {
+
     public static void main (String[] args) throws Exception
     {
-        System.out.println("boombox\n");
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        test testing = new test(500);
+        Set<ConstraintViolation<test>> constraintViolations = validator.validate(testing);
+        if (constraintViolations.size() > 0 )
+        {
+            System.out.println("\n\n<<< Failed validations >>>\n");
+            for (ConstraintViolation<test> constraints : constraintViolations)
+                System.out.println("Error " + constraints.getMessage());
+        }
         Connection conn;
         Statement statement;
         Class.forName("com.mysql.cj.jdbc.Driver");
@@ -24,7 +39,5 @@ public class Main
         statement.executeUpdate(createDB);//statement.executeQuery();
         statement.close();
         conn.close();
-        System.out.println("booopoppo\n");
-
     }
 }
